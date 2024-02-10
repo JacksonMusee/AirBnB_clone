8dc7ed2d3c7483233edc1ebe4b8c0e5e66c2bf86
JacksonMusee <jacksonmutisya94@gmail.com>
2024-02-10 03:40:09 -0800

    Push

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This' where the main class of our website's objects will be defined.
+The BaseModel its methods
+"""
+
+import datetime
+import json
+import uuid
+
+
+class BaseModel:
+    """
+    Defines all common attributes/methods for other classes of the AirBnB
+    clone website
+    """
+    def __init__(self):
+        """
+        Initialization of the instance
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """
+        Out put string when an instance of BaseModel is printed
+        """
+        string = (
+                f"[{self.__class__.__name__}] "
+                f"({self.id}) "
+                f"{self.__dict__}"
+                )
+        return (string)
+
+    def save(self):
+        """
+        Updated updated_at with current time
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance with
+        __class__ added
+        """
+        the_dct = self.__dict__.copy()
+        the_dct['__class__'] = self.__class__.__name__
+        the_dct['created_at'] = self.created_at.isoformat()
+        the_dct['updated_at'] = self.updated_at.isoformat()
+        return (the_dct)
